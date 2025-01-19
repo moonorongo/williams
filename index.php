a5a646ec7e44fc724e78d495ae74f466b0e357fb
Mauro Cifuentes <41113711+maurocifuentes@users.noreply.github.com>
2025-01-19 13:21:28 -0300

    content - wip

--- index.php
+++ index.php
@@ -16,13 +16,18 @@
   <div class="global-container icons y-scroll">
     <div class="container">
       <div class="grid">
-        <?php for ($i=0; $i < 40; $i++) { ?>
-            <div 
-              class="grid-element background-settings back-<?= $i % 10?> js-icon"
-              data-icon-number="<?= $i ?>"
-            ></div>
-          <?php } ?>
-
+          <!--
+            <?php // for ($i=0; $i < 40; $i++) { ?>
+              <div 
+                class="grid-element background-settings back-<?= $i % 10?> js-icon"
+                data-icon-number="<?= $i ?>"
+              ></div>
+            <?php // } ?>
+          -->
+            
+          <?php
+            build_navigation();
+          ?>
           <!-- extra spacing -->
           <div class="grid-element background-settings"></div>
           <div class="grid-element background-settings"></div>
@@ -37,94 +42,9 @@
   <div class="global-container detail">
     <div class="container">
       <div id="detail-container" class="detail-container y-scroll">
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-
-          <h3>Video</h3>
-          <video 
-            loop="" playsinline="" preload="auto" 
-            src="/assets/videos/sample1.mp4">
-          </video>
-
-
-          <h3>Paragraph</h3>
-          <p>
-            This is an example of paragraph text. <br />
-            Eligendi accusantium praesentium officia vel reiciendis sint soluta necessitatibus quasi ipsum dolorum, dicta, iste quos, omnis repellendus mollitia suscipit aperiam quaerat nulla?
-          </p>
-
-          <h3>Unordered list</h3>
-          <ul>
-            <li>The first item in a list</li>
-            <li>Hey, it's the second!</li>
-            <li>What have you heard about the third?</li>
-            <li>Well everone has heard that the third is the word</li>
-          </ul>
-
-          <h3>Ordered list</h3>
-          <ol>
-            <li>The first item in a list</li>
-            <li>Hey, it's the second!</li>
-            <li>What have you heard about the third?</li>
-            <li>Well everone has heard that the third is the word</li>
-          </ol>
-
-          <h3>Video</h3>
-          <video 
-            loop="" playsinline="" preload="auto" 
-            src="/assets/videos/sample1.mp4">
-          </video>
-
-
-          <h3>Blockquote</h3>
-          <blockquote>
-            Wow, this quote is so wonderful. I hope cheese quickly zaps a large mule.
-          </blockquote>
-
-          <h3>Article</h3>
-          <article>
-            <header>
-              <h3>
-                This is header inside an <code>article</code>.
-              </h3>
-            </header>
-
-            <p>
-              And a <code>paragraph</code> following the header
-            </p>
-          </article>
-          
-
-
-          <!-- EXTRA TEST -->
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-          
-          <h3>Paragraph</h3>
-          <p>
-            This is an example of paragraph text. <br />
-            Eligendi accusantium praesentium officia vel reiciendis sint soluta necessitatibus quasi ipsum dolorum, dicta, iste quos, omnis repellendus mollitia suscipit aperiam quaerat nulla?
-          </p>
-
-          <h3>Unordered list</h3>
-          <ul>
-            <li>The first item in a list</li>
-            <li>Hey, it's the second!</li>
-            <li>What have you heard about the third?</li>
-            <li>Well everone has heard that the third is the word</li>
-          </ul>
-
-          <h3>Ordered list</h3>
-          <ol>
-            <li>The first item in a list</li>
-            <li>Hey, it's the second!</li>
-            <li>What have you heard about the third?</li>
-            <li>Well everone has heard that the third is the word</li>
-          </ol>
-
-
+        <?php
+          // build_details();
+        ?>
 
         <div class="nav-buttons-spacer"></div>
       </div>
