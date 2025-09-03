f4a4a34af67af281240be3f5c455eebcb00695ad
Mauro Cifuentes <41113711+maurocifuentes@users.noreply.github.com>
2025-09-02 21:07:18 -0300

    ajustes css

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Convergencia</title>
+  <link rel="stylesheet" href="/assets/css/vars.css">
   <link rel="stylesheet" href="/assets/css/normalize.css">
   <link rel="stylesheet" href="/assets/css/fonts.css">
   <link rel="stylesheet" href="/assets/css/typebase.css">
   <link rel="stylesheet" href="/assets/css/swipe-scroller.css">
+  <link rel="stylesheet" href="/assets/css/containers.css">
+  <link rel="stylesheet" href="/assets/css/nav-grid.css">
+  
 
   <style>
     img {
@@ -23,17 +27,84 @@
 <body>
   <!-- CONTENIDOS -->
   <div class="global-container">
-    <div class="container">
-      <!-- NAV -->
-       <!-- PROBAR MAÑANA CON UNA IMPLEMENTACION PROPIA -->
-      <div class="detail-container swipe-scroller" id="SmoothScroll" data-component="SwipeScroller">
-        <div class="track">
-          <?php include("content/03/content.html"); ?>
-        </div>
-      </div>
-    
+    <!-- TOP BAR -->
+    <div class="top-bar-container">
+      top bar contents
     </div>
-  </div>
+
+    <!-- MAIN NAV -->
+    <div class="main-nav-container">
+      <div class="grid-container">
+        
+        <div class="grid-row">
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Cámara</div>
+          </div>
+        
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Email</div>
+          </div>
+
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Contactos</div>
+          </div>
+        </div>
+
+        <div class="grid-row">
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Calculadora</div>
+          </div>
+
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Juegos</div>
+          </div>
+
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Mapas</div>
+          </div>
+        </div>
+
+        <div class="grid-row">
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Música</div>
+          </div>
+
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Teléfono</div>
+          </div>
+
+          <div class="grid-item">
+            <div class="icon-container"></div>
+            <div class="text-container">Videos</div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    
+    <!-- MAiN CONTENT -->
+    <? /* 
+    <div class="detail-container swipe-scroller" id="SmoothScroll" data-component="SwipeScroller">
+      <div class="track">
+        <?php include("content/03/content.html"); ?>
+      </div>
+    </div>
+    */ ?>
+
+    <!-- BOTTOM BAR -->
+    <div class="bottom-bar-container">
+      bottom bar contents
+    </div>
+
+  </div><!-- .global-container -->
 
 
 </body>
